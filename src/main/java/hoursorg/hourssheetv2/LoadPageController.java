580ad61b8202ce411702ffcffdd820d0cfc56bdc
Joseph-Bond-GitHub <152449724+Joseph-Bond-GitHub@users.noreply.github.com>
2026-02-25 20:17:31 +0000

    Added files to contain code to load from a file

---
+++ src/main/java/hoursorg/hourssheetv2/LoadPageController.java
@@ -0,0 +1,20 @@
+package hoursorg.hourssheetv2;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+
+public class LoadPageController {
+    @FXML
+    protected Label monthlbl;
+
+    private String fileName;
+    private String fileData;
+
+    private void initialize(){
+        //get array of data from text file
+        //Add and populate fields of gridPane with the relevant data
+    }
+
+
+}
