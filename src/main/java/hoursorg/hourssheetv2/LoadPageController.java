09221901ce1c56683f6eab8d57484a6a2b41943b
Joseph-Bond-GitHub <152449724+Joseph-Bond-GitHub@users.noreply.github.com>
2026-02-27 10:53:45 +0000

    Outline some method shells in LoadPageController. Added Filechc to scene

--- src/main/java/hoursorg/hourssheetv2/LoadPageController.java
+++ src/main/java/hoursorg/hourssheetv2/LoadPageController.java
@@ -1,12 +1,18 @@
 package hoursorg.hourssheetv2;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
 
 
 public class LoadPageController {
     @FXML
     protected Label monthlbl;
+    @FXML
+    protected ChoiceBox<String> filechc;
+    @FXML
+    protected GridPane datagridPane;
 
     private String fileName;
     private String fileData;
@@ -16,5 +22,8 @@ public class LoadPageController {
         //Add and populate fields of gridPane with the relevant data
     }
 
+    private String[] getFileData(){
+
+    }
 
 }
